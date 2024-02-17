57c7fb3fd3197c7367789c7819dde4368379fdfb
Ttst <thibautstachnick@gmail.com>
2024-02-17 15:46:30 +0100

    add a function that detect prime number

--- main.cpp
+++ main.cpp
@@ -47,7 +47,60 @@ int exchangeTwoVariables(){
     return 0;
 }
 
+bool isNumberPrime(){
+
+    int n;
+    std::cout << "Définissez un nombre n pour savoir si il est premier ou non";
+    std::cin >> n;
+
+    int j;
+
+    for (j = 2; j <= n-1; j++) {
+        if (n%j==0){
+            break;
+        }
+    }
+    if (j> sqrt(n)){
+        std::cout << "Premier";
+    } else{
+        std::cout << "Pas premier";
+    }
+
+    return true;
+}
+
+
+bool checkIfVInArray(){
+
+    int n;
+    int t[n];
+    bool b = false;
+
+    do {
+        std::cout << "Donnez une valeur à n";
+        std::cin >> n;
+    } while (n <=9);
+
+
+    for (int j = 0; j <= n; j++) {
+        std::cout << "Donnez une valeur pour t["<< j <<"]";
+        std::cin >> t[j];
+    }
+
+    for (int i = 0; i <= 10; i++) {
+        if (t[i] >=1 && t[i] <= 10){
+            b = true;
+        }
+    }
+    if (b){
+        std::cout << "Il existe une valeur entre 1 e 10 dans les 10 premieres valeurs du tableau t";
+    }
+
+    std::cout << b;
+    return b;
+}
+
 int main() {
-    exchangeTwoVariables();
+    isNumberPrime();
     return 0;
 }
